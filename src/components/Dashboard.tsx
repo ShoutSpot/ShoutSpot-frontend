6a36b371f7bf0672fc0aeb0fbfb498a1acf3533e
Atulgadhari27 <atulgadhari27@gmail.com>
2024-11-09 22:18:34 +0530

    Spaces searchbar done

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { OverviewCard } from "./OverviewCard"
+import { Spaces } from "./Spaces"
 
 export const Dashboard = () => {
     return (
@@ -11,22 +12,7 @@ export const Dashboard = () => {
                 </div>
             </div>
 
-            <div className="max-w-6xl mx-auto px-4 sm:px-6">
-                <div className="py-12 md:py-20">
-                    <div className="flex-1 flex items-center justify-between pb-5">
-                        <div className="text-3xl text-white font-semibold tracking-wide ">Spaces</div>
-                    </div>
-                    <div className="text-center py-12 bg-gray-800 rounded-lg border border-gray-700 shadow-sm">
-                        <img src="/new-folder.png" className="w-10 h-10 mx-auto text-gray-400 mb-4" ></img>
-                        <h3 className="text-xl font-semibold text-white mb-2">No spaces yet</h3>
-                        <p className="text-gray-400 mb-6">Create your first space to start collecting testimonials</p>
-                        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-2" type="button" aria-haspopup="true" aria-expanded="false">
-                            <span className="flex text-sm">
-                            <img src="/plus-icon.png" className="h-5 w-5 mr-2" ></img>Create a new space</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <Spaces/>
         </div>
     )
 }
