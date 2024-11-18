1ee1d2593cd9a9e965d8b30215ec3eb942994e2a
shreyas200110 <shreyaspatil200110@gmail.com>
2024-11-18 21:59:39 +0530

    completed single review component

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ function App() {
     <div className='bg-gray-900 w-full h-full'>
       <div className='mx-auto px-4 sm:px-8 lg:px-16'>
         <Navbar2/>
-        {/* <div className='flex justify-around' >
+        <div className='flex justify-around' >
           <Dashboard/>
-        </div> */}
+        </div>
         <Footer/>
-        {/* <div className='w-screen h-screen block text-center '>
+        <div className='w-screen h-screen block text-center '>
           <CreateSpaceModal/>
-        </div> */}
-        <SingleReview reviewType='Text' positiveStarsCount={3} reviewVideo='hello' reviewText='Hi you all good' reviewImage='public/userlogo.png' userDetails={{name : 'Shreyas', companyName: "NielsenIQ", email:'shreyas.patil@nielseniq.com', address: 'pune', socialLink:'linkedin', submitDateTime: '19th Nov 2024'}} />
+        </div>
+        {/* <SingleReview reviewType='Text' positiveStarsCount={3} reviewVideo='hello' reviewText='Hi you all good' reviewImage='public/userlogo.png' userDetails={{name : 'Shreyas', companyName: "NielsenIQ", email:'shreyas.patil@nielseniq.com', address: 'pune', socialLink:'linkedin', submitDateTime: '19th Nov 2024'}} /> */}
         
       </div>
     </div>
