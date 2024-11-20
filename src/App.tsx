8cef0964ea1c1c37b94c82be0c1510a775e8e08d
shreyas200110 <shreyaspatil200110@gmail.com>
2024-11-20 14:55:23 +0530

    landing page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,94 @@
-import { Dashboard } from './components/Dashboard'
-import './App.css'
-import { Navbar2 } from './components/Navbar2'
-import { Footer } from './components/Footer'
-import { CreateSpaceModal } from './components/CreateSpaceModal'
+import { useEffect } from 'react';
+import { Dashboard } from './components/Dashboard';
+import './App.css';
+import { Navbar2 } from './components/Navbar2';
+import { Footer } from './components/Footer';
+import { CreateSpaceModal } from './components/CreateSpaceModal';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SingleReview } from './components/SingleReview'
-import { EmbedSingleTestimonial } from './components/EmbedSingleTestimonial'
-import { SignIn } from './components/SignupSignin/SignIn'
-import { Signup } from './components/SignupSignin/Signup'
-import { SpaceDashboard } from './components/SpaceDetails/SpaceDashboard'
+import { EmbedSingleTestimonial } from './components/EmbedSingleTestimonial';
+import { SignIn } from './components/SignupSignin/SignIn';
+import { Signup } from './components/SignupSignin/Signup';
+import { SpaceDashboard } from './components/SpaceDetails/SpaceDashboard';
+import { LandingPage } from './components/LandingPage/LandingPage';
 
 function App() {
+  useEffect(() => {
+    document.documentElement.classList.add('dark'); // Apply dark theme on load
+  }, []);
 
   return (
-
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <div className='bg-gray-900 w-full h-full'>
-            <div className='mx-auto px-4 sm:px-8 lg:px-16'>
-              <Navbar2 />
-              <div className='flex justify-around' >
-                <Dashboard />
-              </div>
-              <Footer />
-              <div className='w-screen h-screen block text-center '>
-                <CreateSpaceModal />
-              </div>
-              <EmbedSingleTestimonial />
-            </div>
-          </div>} />
-
-        <Route path="/signin" element={
-          <div className='bg-gray-900 w-full h-full'>
-            <div className='mx-auto px-4 sm:px-8 lg:px-16'>
-              <Navbar2 />
-              <div className='flex justify-around' >
-                <SignIn />
+        <Route
+          path="/"
+          element={
+            <div className="bg-gray-900 w-full h-full App">
+              <div className="mx-auto px-4 sm:px-8 lg:px-16">
+                <Navbar2 />
+                <div className="flex justify-around">
+                  <Dashboard />
+                </div>
+                <Footer />
+                <div className="w-screen h-screen block text-center">
+                  <CreateSpaceModal />
+                </div>
+                <EmbedSingleTestimonial />
               </div>
             </div>
-          </div>} />
-
-        <Route path="/signup" element={
-          <div className='bg-gray-900 w-full h-full'>
-            <div className='mx-auto px-4 sm:px-8 lg:px-16'>
-              <Navbar2 />
-              <div className='flex justify-around' >
-                <Signup />
+          }
+        />
+        <Route
+          path="/landingpage"
+          element={
+            <div className="bg-gray-900 w-full h-full App">
+              <div className="mx-auto px-4 sm:px-8 lg:px-16">
+                <Navbar2 />
+                <LandingPage/>
+                <Footer />
               </div>
             </div>
-          </div>} />
-        <Route path="/product/space" element={
-          <div className='bg-gray-900 w-full h-full'>
-            <div className='mx-auto px-4 sm:px-8 lg:px-16'>
-              <Navbar2 />
-              <SpaceDashboard />
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <div className="bg-gray-900 w-full h-full App">
+              <div className="mx-auto px-4 sm:px-8 lg:px-16">
+                <Navbar2 />
+                <div className="flex justify-around">
+                  <SignIn />
+                </div>
+              </div>
             </div>
-          </div>} />
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <div className="bg-gray-900 w-full h-full App">
+              <div className="mx-auto px-4 sm:px-8 lg:px-16">
+                <Navbar2 />
+                <div className="flex justify-around">
+                  <Signup />
+                </div>
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/product/space"
+          element={
+            <div className="bg-gray-900 w-full h-full App">
+              <div className="mx-auto px-4 sm:px-8 lg:px-16">
+                <Navbar2 />
+                <SpaceDashboard />
+              </div>
+            </div>
+          }
+        />
       </Routes>
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
